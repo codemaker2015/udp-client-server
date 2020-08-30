7fca7b86accff1afe492fcb634017571301df3dc
Vishnu Sivan <codemaker2015@gmail.com>
2020-08-30 11:26:59 +0530

    Changes

--- udp.js
+++ udp.js
@@ -54,36 +54,36 @@ server.bind(5009);
 
 // -------------------- udp client ----------------
 
-// var buffer = require('buffer');
+var buffer = require('buffer');
 
-// // creating a client socket
-// var client = udp.createSocket('udp4');
+// creating a client socket
+var client = udp.createSocket('udp4');
 
-// //buffer msg
-// var data = Buffer.from('siddheshrane');
+//buffer msg
+var data = Buffer.from('siddheshrane');
 
-// client.on('message',function(msg,info){
-//   console.log('Data received from server : ' + msg.toString());
-//   console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
-// });
+client.on('message',function(msg,info){
+  console.log('Data received from server : ' + msg.toString());
+  console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
+});
 
-// //sending msg
-// client.send(data,2222,'localhost',function(error){
-//   if(error){
-//     client.close();
-//   }else{
-//     console.log('Data sent !!!');
-//   }
-// });
+//sending msg
+client.send(data,2222,'localhost',function(error){
+  if(error){
+    client.close();
+  }else{
+    console.log('Data sent !!!');
+  }
+});
 
-// var data1 = Buffer.from('hello');
-// var data2 = Buffer.from('world');
+var data1 = Buffer.from('hello');
+var data2 = Buffer.from('world');
 
-// //sending multiple msg
-// client.send([data1,data2],2222,'localhost',function(error){
-//   if(error){
-//     client.close();
-//   }else{
-//     console.log('Data sent !!!');
-//   }
-// });
+//sending multiple msg
+client.send([data1,data2],2222,'localhost',function(error){
+  if(error){
+    client.close();
+  }else{
+    console.log('Data sent !!!');
+  }
+});
